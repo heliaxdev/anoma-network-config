6eb83d5651603b9ac090ffc426d5f82919729bb5
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2022-01-25 15:47:15 +0100

    [feat]: include chain-id as a second argument

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@
 # Must have authenticated `gh` available (see https://github.com/cli/cli) with
 # write access to https://github.com/heliaxdev/anoma-network-config.
 #
-# The first argument is the name of the archive file which must be present in 
-# the current working directory. The archive's name is expected to be equal to
-# the chain ID with a "tar.gz" suffix.
+# The first argument is the path to the archive file. 
+# The archive's name is expected to be equal to the chain ID with a "tar.gz" suffix.
+# The second argument is the chain ID.
 #
 # IMPORTANT: Use pre-release for the devnet, add `--prerelease` flag at the end 
 # when calling this script.
@@ -19,8 +19,8 @@
 set -e
 
 ARCHIVE_FILE_PATH=$1
-CHAIN_ID=${ARCHIVE_FILE_PATH%".tar.gz"}
-EXTRA_ARG=$2
+CHAIN_ID=$2
+EXTRA_ARG="${@:3}"
 echo "Chain ID: $CHAIN_ID"
 CWD=$(pwd)
 ARCHIVE="$CWD/$ARCHIVE_FILE_PATH"
@@ -32,7 +32,7 @@ if [ -n "$EXTRA_ARG" ] ; then
         --title "$CHAIN_ID" \
         --notes "Released network with chain ID: $CHAIN_ID" \
         --repo heliaxdev/anoma-network-config \
-        "$EXTRA_ARG"
+        $EXTRA_ARG
 else
     gh release create "$CHAIN_ID" "$ARCHIVE" \
         --title "$CHAIN_ID" \
